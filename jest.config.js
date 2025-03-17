0f6c665dceb571b8c57a6cff335f3e753e0968a0
arnabsom <Orlando12!@>
2025-03-17 14:55:22 -0400

    modified

--- jest.config.js
+++ jest.config.js
@@ -3,17 +3,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
-    '^.+\\.(js|ts|tsx)$': ['ts-jest'
-  ]
+    '^.+\\.(js|ts|tsx)$': 'ts-jest'
   },
   transformIgnorePatterns: [
-  "\\node_modules\\@microsoft\\sp-dialog",
-  "\\node_modules\\@microsoft\\sp-core-library",
-  "node_modules/(?!sp-core-library)",
-  "node_modules/(?!@microsoft/sp-core-library)"
+'/node_modules/(?!your-module-name).+\\.js$',
 ],
   testMatch: ['**/__tests__/**/*.(test|spec).(ts|tsx)'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
